af0408f159cfb20ccf1853d84810d863de6bd410
maguowei <maguowei@meituan.com>
2016-12-29 16:39:57 +0800

    pull down refresh

--- pages/galary/galary.js
+++ pages/galary/galary.js
@@ -14,6 +14,18 @@ Page({
       videoList: videoList
     })
   },
+  onPullDownRefresh: function () {
+    console.log('onPullDownRefresh', new Date())
+    let videoList = this.getVideoList(true)
+    let that = this
+    wx.stopPullDownRefresh({
+      complete: function(res) {
+        that.setData({
+          videoList: videoList
+        })
+      }
+    })
+  },
   videoLike: function(event) {
     console.log(event)
     console.log('video ' + event.currentTarget.dataset.videoId + ' liked')
@@ -22,14 +34,18 @@ Page({
     console.log(event)
     console.log('video ' + event.currentTarget.dataset.videoId + ' shared')
   },
-  getVideoList: function() {
-    return [{
+  getVideoList: function(flag) {
+    return !flag ? [{
       id: 1,
       content: 'video content ...',
       src: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
     }, {
       id: 2,
       content: 'another video content ...',
+      src: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
+    }] : [{
+      id: 3,
+      content: 'refresh video content ...',
       src: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
     }];
   }
